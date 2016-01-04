f1fc719e7af528083ede3c6c7f7e806c23d501f1
Lauryn Davis and Riley Starnes <riley@>
2016-01-04 11:06:43 -0800

    pass first spec

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,5 +1,10 @@
 class Fixnum
   define_method(:ping_pong) do
-    # code here
-  end
+    new_numbers = []
+    self.times() do |number|
+      new_number = number.+(1)
+      new_numbers.push(new_number)
+    end
+    new_numbers
+end
 end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -1,6 +1,8 @@
 require('rspec')
 require('ping_pong')
 
-describe('String#title_case') do
-  # test here
+describe('Fixnum#ping_pong') do
+  it("counts from one to the given number") do
+    expect((2).ping_pong()).to(eq([1,2]))
+  end
 end
