b205e33178825e5ff435021d1153c784e52f647b
Lauryn Davis and Riley Starnes <riley@>
2016-01-04 11:36:07 -0800

    pass fourth spec

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -3,7 +3,9 @@ class Fixnum
     new_numbers = []
     self.times() do |number|
     new_number = number.+(1)
-      if new_number.%(3).eql?(0)
+      if new_number.%(15).eql?(0)
+        new_numbers.push("ping pong")
+      elsif new_number.%(3).eql?(0)
         new_numbers.push("ping")
       elsif new_number.%(5).eql?(0)
         new_numbers.push("pong")
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -11,4 +11,7 @@ describe('Fixnum#ping_pong') do
   it("returns pong when a number is divisible by 5") do
     expect((5).ping_pong()).to(eq([1,2,"ping",4,"pong"]))
   end
+  it("returns ping pong when a number is divisible by both 5 and 3") do
+    expect((15).ping_pong()).to(eq([1,2,"ping",4,"pong","ping",7,8,"ping","pong",11,"ping",13,14,"ping pong"]))
+  end
 end
