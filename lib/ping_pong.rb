f11acd20c33e117c7feaf40977527a88329cbf21
Lauryn Davis and Riley Starnes <riley@>
2016-01-04 10:26:16 -0800

    initial commit

---
+++ lib/ping_pong.rb
@@ -0,0 +1,5 @@
+class Fixnum
+  define_method(:ping_pong) do
+    # code here
+  end
+end
---
+++ spec/ping_pong_spec.rb
@@ -0,0 +1,6 @@
+require('rspec')
+require('ping_pong')
+
+describe('String#title_case') do
+  # test here
+end
