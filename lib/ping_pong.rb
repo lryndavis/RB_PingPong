b3f644fc68ef4fcfd2a6b8068ec913ee1c10a6d5
Lauryn Davis and Riley Starnes <riley@>
2016-01-04 11:26:24 -0800

    pass second and third spec

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -2,9 +2,15 @@ class Fixnum
   define_method(:ping_pong) do
     new_numbers = []
     self.times() do |number|
-      new_number = number.+(1)
-      new_numbers.push(new_number)
+    new_number = number.+(1)
+      if new_number.%(3).eql?(0)
+        new_numbers.push("ping")
+      elsif new_number.%(5).eql?(0)
+        new_numbers.push("pong")
+      else
+        new_numbers.push(new_number)
+      end
     end
     new_numbers
-end
+  end
 end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -5,4 +5,10 @@ describe('Fixnum#ping_pong') do
   it("counts from one to the given number") do
     expect((2).ping_pong()).to(eq([1,2]))
   end
+  it("returns ping when a number is divisible by 3") do
+    expect((3).ping_pong()).to(eq([1,2,"ping"]))
+  end
+  it("returns pong when a number is divisible by 5") do
+    expect((5).ping_pong()).to(eq([1,2,"ping",4,"pong"]))
+  end
 end
